bd296ce192129fa4a3865648c528f0e074f5e880
Jan Claußen <jan.claussen10@web.de>
2026-01-28 12:52:26 +0100

    Reorder tests

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "matrix.h"
 #include "tests.h"
 
 int main(void)
@@ -6,58 +7,74 @@ int main(void)
 
 	/* int matrices */
 
-	struct matrix *A = mat_alloc(3, 3);
-	struct matrix *B = mat_alloc(3, 3);
+	TEST("creation of A1 on the stack");
+	MATRIX(A1, 3, 3);
+	mat_set(A1, 0, 0, 1);
+	mat_set(A1, 0, 1, 1);
+	mat_set(A1, 0, 2, 1);
+	jl_test_mat(A1, "A1 = [1 1 1; 0 0 0; 0 0 0]");
 
-	mat_set(A, 0, 0, 1);
-	mat_set(A, 0, 1, 1);
-	mat_set(A, 0, 2, 1);
+	TEST("creation of B1 on the stack");
+	MATRIX(B1, 3, 3);
+	mat_set(B1, 0, 0, 1);
+	mat_set(B1, 1, 0, 1);
+	mat_set(B1, 2, 0, 1);
+	jl_test_mat(B1, "B1 = [1 0 0; 1 0 0; 1 0 0]");
 
-	TEST("A");
-	jl_test_mat(A, "A = [1 1 1; 0 0 0; 0 0 0]");
+	TEST("multiplication of A1 * B1 ");
+	MAT_MUL(C1, A1, B1);
+	jl_test_mat(C1, "A1 * B1");
 
-	mat_set(B, 0, 0, 1);
-	mat_set(B, 1, 0, 1);
-	mat_set(B, 2, 0, 1);
+	TEST("creation of A2 on the heap");
+	struct matrix *A2 = mat_alloc(3, 3);
+	mat_set(A2, 0, 0, 1);
+	mat_set(A2, 0, 1, 1);
+	mat_set(A2, 0, 2, 1);
+	jl_test_mat(A2, "A2 = [1 1 1; 0 0 0; 0 0 0]");
 
-	TEST("B");
-	jl_test_mat(B, "B = [1 0 0; 1 0 0; 1 0 0]");
+	TEST("creation of B2 on the heap");
+	struct matrix *B2 = mat_alloc(3, 3);
+	mat_set(B2, 0, 0, 1);
+	mat_set(B2, 1, 0, 1);
+	mat_set(B2, 2, 0, 1);
+	jl_test_mat(B2, "B2 = [1 0 0; 1 0 0; 1 0 0]");
 
-	struct matrix *C = mat_mul(A, B);
-	TEST("C");
-	jl_test_mat(C, "A * B");
+	TEST("multiplication of A2 * B2 ");
+	struct matrix *C2 = mat_mul(A2, B2);
+	jl_test_mat(C2, "A2 * B2");
 
-	mat_delete(A);
-	mat_delete(B);
-	mat_delete(C);
+	mat_delete(A2);
+	mat_delete(B2);
+	mat_delete(C2);
 
-	/* float matrices */
+	TEST("shifting of A");
+	mat_reset(A1);
+	mat_set(A1, 1, 1, 1);
+	mat_print(A1);
 
-	struct fmatrix *fA = fmat_alloc(3, 3);
-	struct fmatrix *fB = fmat_alloc(3, 3);
+	printf("East: \n");
+	mat_reset(A1);
+	mat_set(A1, 1, 1, 1);
+	mat_shift_east(A1, 1);
+	mat_print(A1);
 
-	fmat_set(fA, 0, 0, 1.0);
-	fmat_set(fA, 0, 1, 1.0);
-	fmat_set(fA, 0, 2, 1.0);
+	printf("West: \n");
+	mat_reset(A1);
+	mat_set(A1, 1, 1, 1);
+	mat_shift_west(A1, 1);
+	mat_print(A1);
 
-	TEST("fA");
-	jl_test_fmat(fA, "fA = [1. 1. 1.; 0. 0. 0.; 0. 0. 0.]", 1e-12, 1e-12);
+	printf("North: \n");
+	mat_reset(A1);
+	mat_set(A1, 1, 1, 1);
+	mat_shift_north(A1, 1);
+	mat_print(A1);
 
-	fmat_set(fB, 0, 0, 1.0);
-	fmat_set(fB, 1, 0, 1.0);
-	fmat_set(fB, 2, 0, 1.0);
-
-	TEST("fB");
-	jl_test_fmat(fB, "fB = [1. 0. 0.; 1. 0. 0.; 1. 0. 0.]", 1e-12, 1e-12);
-
-	struct fmatrix *fC = fmat_mul(fA, fB);
-
-	TEST("fC");
-	jl_test_fmat(fC, "fC = fA * fB", 1e-12, 1e-12);
-
-	fmat_delete(fA);
-	fmat_delete(fB);
-	fmat_delete(fC);
+	printf("South: \n");
+	mat_reset(A1);
+	mat_set(A1, 1, 1, 1);
+	mat_shift_south(A1, 1);
+	mat_print(A1);
 
 	jl_atexit_hook(0);
 
